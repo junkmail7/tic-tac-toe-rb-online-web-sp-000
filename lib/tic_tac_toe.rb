38c95d2c3394157138409d86c8d375c7f8dc2edc
William Pannell <lpann5@gmail.com>
2020-10-30 21:43:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,7 +35,24 @@ def turn(board)
   end
 end
 
-# Define your play method below
+def turn_count(board)
+  turns = 0
+  board.each do |x|
+    if x == "X" || x== "O"
+      turns += 1
+    end
+  end
+  return turns
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
+
 def play(board)
   x = 0
   while x != 9
